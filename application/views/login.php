85e277d89ebfd4ee52e485460ffeafb148b5cc31
Nekozawaa <47913028+Nekozawaa@users.noreply.github.com>
2019-04-29 10:05:35 +0700

    login dan signupp
    menambahkan view login dan signup

--- application/views/login.php
+++ application/views/login.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html>
 <head>
   <title>Epidemic Sound</title>
@@ -11,7 +11,7 @@
 <!-- Include the above in your HEAD tag -->
 
 
-<div style="text-align: center; padding-top: 50px;">
+ <div style="text-align: center; padding-top: 50px;">
   <img src="assets/img/logo.png">
 </div>
 
@@ -26,11 +26,16 @@
     </div>
 
     <!-- Login Form -->
-    <form>
-      <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
-      <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+    <form action="<?= site_url('login') ?>" method="post">
+        <div style="text-align : center;">
+            <?php if(isset($_SESSION)) {
+                echo $this->session->flashdata('flash_data');
+            } ?>
+        </div>
+      <input type="text" id="login" class="fadeIn second" name="username" placeholder="login">
+      <input type="text" id="password" class="fadeIn third" name="password" placeholder="password">
       <input type="submit" class="fadeIn fourth" value="Log In">
-      <p>Don't have an account yet?</p> <a style="color: skyblue" href="#"> Sign up here</a>
+      <p>Don't have an account yet?</p> <a style="color: skyblue" href="<?= site_url('login/signup') ?>"> Sign up here</a>
     </form>
 
     <!-- Remind Passowrd -->
